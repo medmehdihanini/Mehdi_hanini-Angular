c24cdf1901cc8c21fa98b96b00d8fe3018600d49
medmehdihanini <80129416+medmehdihanini@users.noreply.github.com>
2023-10-21 13:30:00 +0100

    first push new repositry

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FournisseurComponent } from './DATABINDING-WORKSHOP/fournisseur/fournisseur.component';
+import { ListFournisseurComponent } from './DATABINDING-WORKSHOP/list-fournisseur/list-fournisseur.component';
+import { ListProduitsComponent } from './DATABINDING-WORKSHOP/list-produits/list-produits.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'atelier1',
+    component: FournisseurComponent,
+  },
+  {
+    path: 'atelier1exercice2',
+    component: ListFournisseurComponent,
+  },
+  {
+    path: 'atelier2',
+    component: ListProduitsComponent,
+  },
+  {
+    path: 'atelier3',loadChildren:()=>import('./workshop-routing/workshop-routing.module')
+    .then(mod=> mod.WorkshopRoutingModule )
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FournisseurComponent } from './DATABINDING-WORKSHOP/fournisseur/fournisseur.component';
+import { ListFournisseurComponent } from './DATABINDING-WORKSHOP/list-fournisseur/list-fournisseur.component';
+import { ListProduitsComponent } from './DATABINDING-WORKSHOP/list-produits/list-produits.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FournisseurComponent,
+    ListFournisseurComponent,
+    ListProduitsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
